f290e003c8a71bf012b48e310a4702e01b6acada
DanielH7-ctrl <danielhuerta826@gmail.com>
2022-03-05 14:39:10 -0500

    Integracion de API

--- app/src/main/java/com/example/appclima/MainActivity.kt
+++ app/src/main/java/com/example/appclima/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.appclima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +29,15 @@ class MainActivity : AppCompatActivity() {
 
         val ciudad = intent.getStringExtra("com.example.appclima.ciudades.CIUDAD")
         Toast.makeText(this, ciudad, Toast.LENGTH_SHORT).show()
+
+        if(Network.isNetworkAvailable(this)){
+            //EJECUTAR SOLICITUD HTTP
+            solicitudHTTPVolley("http://api.openweathermap.org/data/2.5/weather?id=3527639&appid=a0bb28f0216d0f9427c2ea0e58fe124c")
+            //a0bb28f0216d0f9427c2ea0e58fe124c
+            //Felipe Carrillo Puerto 3527639
+        }else {
+            //MOSTRAR MENSAJE DE ERROR
+        }
 
         val ciudadfcp = Ciudad("Felipe Carrillo Puerto", 27, "Soleado")
         val ciudadtul = Ciudad("Tulum", 24, "Soleado")
@@ -51,7 +66,18 @@ class MainActivity : AppCompatActivity() {
                 tvEstatus?.text = ciudadpla.estatus
             }
         }
-
-
     }
+    private fun solicitudHTTPVolley(url: String) {
+        val queue = Volley.newRequestQueue(this)
+
+        val solicitud = StringRequest(Request.Method.GET, url, Response.Listener<String> {
+                response ->
+            try {
+                Log.d( "solicitudHTTPVolley", response)
+            } catch (e: Exception) {
+            }
+        }, Response.ErrorListener {})
+        queue.add(solicitud)
+    }
+
 }
